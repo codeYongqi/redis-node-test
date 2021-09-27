65f93141e428b3c09eaff0f415f601093d06e4a7
kirin <zhuyongqi9@gmail.com>
2021-09-27 15:47:57 +0800

    Update insertJson.js
    add crud

--- hash/insertJson.js
+++ hash/insertJson.js
@@ -6,6 +6,7 @@ const util = require('util');
 redisClient.hset = util.promisify(redisClient.hset);
 redisClient.hget = util.promisify(redisClient.hget);
 redisClient.hgetall = util.promisify(redisClient.hgetall);
+redisClient.del = util.promisify(redisClient.del);
 
 const geneTargetData = function generateTargetDataByFaker() {
   let target = {};
@@ -22,36 +23,90 @@ const geneTargetData = function generateTargetDataByFaker() {
 }
 
 const insertJsonData = async function insertJsonDataByString() {
-  let start = performance.now();
+  let cost = 0;
 
   for(let i = 280001; i <= 290001; i++) {
     console.log(i);
     let data = geneTargetData();
+
+    let start = performance.now();
     for (const key in data) {
       if (Object.hasOwnProperty.call(data, key)) {
         const element = data[key];
         await redisClient.hset(i, key, element); 
       }
     }
+    let end = performance.now();
+    cost += end - start;
   }
-
-  let end = performance.now();
-  console.log(`it cost ${end - start} thrillseconds`);
+  console.log(`it cost ${cost} thrillseconds`);
+  console.log(`average cost ${(cost)/10000 } thrillseconds`);
 }
 
 const findJsonData = async function findStringJsonData() {
-  let start = performance.now();
+  let cost = 0;
   for(let i = 280001; i <= 290000; i++) {
     console.log(i);
+    let start = performance.now();
     let data = await redisClient.hgetall(i);
+    let end = performance.now();
+    cost += end - start;
     console.log(data);
   }
-  let end = performance.now();
-  console.log(`it cost ${end - start} thrillseconds`);
-  console.log(`average cost ${(end - start) / 10000 } thrillseconds`);
+  console.log(`it cost ${cost} thrillseconds`);
+  console.log(`average cost ${(cost)/10000 } thrillseconds`);
 }
 
-findJsonData();
+const findJsonOneField = async function findOneFieldInJsonData() {
+  let cost = 0;
+  for(let i = 280001; i <= 290000; i++) {
+    console.log(i);
 
-// insertJsonData();
+    let start = performance.now();
+    let data = await redisClient.hget(i, 'url');
+    let end = performance.now();
+
+    cost += end - start;
+    console.log(data);
+  }
+  console.log(`it cost ${cost} thrillseconds`);
+  console.log(`average cost ${(cost)/10000 } thrillseconds`);
+}
+
+const updateJson = async function updateOneFileInJson() {
+  let cost = 0;
+  for(let i = 280001; i <= 290000; i++) {
+    console.log(i);
+
+    let url = faker.internet.url();
+
+    let start = performance.now();
+    await redisClient.hset(i,'url', url);
+    let end = performance.now();
+    cost += end - start;
+  }
+  console.log(`it cost ${cost} thrillseconds`);
+  console.log(`average cost ${(cost)/10000 } thrillseconds`);
+}
+
+
+const deleteJson = async function deleteJsonData() {
+  let cost = 0;
+  for(let i = 280001; i <= 290000; i++) {
+    console.log(i);
+
+    let start = performance.now();
+    await redisClient.del(i);
+    let end = performance.now();
+    cost += end - start;
+  }
+  console.log(`it cost ${cost} thrillseconds`);
+  console.log(`average cost ${(cost)/10000 } thrillseconds`);
+}
+
+//  insertJsonData();
+// findJsonData();
+// findJsonOneField();
+// updateJson();
+ deleteJson();
 
