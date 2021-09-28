b845deade7ed35d7c8da7afa4c40f64d496f72ba
kirin <zhuyongqi9@gmail.com>
2021-09-28 15:56:21 +0800

    Update client.js
     add redisearch module

--- client.js
+++ client.js
@@ -1,7 +1,9 @@
 const redis = require("redis");
 const redisearch = require('redis-redisearch');
+const rejson = require('redis-rejson');
 
 redisearch(redis);
+rejson(redis);
 const redisClient= redis.createClient();
 
 redisClient.on("error", function(error) {
